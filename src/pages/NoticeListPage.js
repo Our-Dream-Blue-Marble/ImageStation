0c30c582c08b4d748520fb3af734aa185eb4e633
ssuni00 <run9986@naver.com>
2023-07-03 17:36:21 +0900

    feat: 게시물 pagination적용
    게시물 5개씩 끊어서 pagination 적용.
    widgets폴더에 NoticePagination추가
    
    (#2)

--- src/pages/NoticeListPage.js
+++ src/pages/NoticeListPage.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import fbase, { dbService } from "fbase";
+import NoticePagination from "../widgets/NoticePagination";
+import styled from "styled-components";
 
 const NoticeListPage = () => {
   const [notice, setNotice] = useState([]);
-  const [page, setPage] = useState([]);
+  const [page, setPage] = useState(1);
+  const [notices, setNotices] = useState(5);
+  const offset = (page - 1) * notices;
 
   useEffect(() => {
     dbService
@@ -19,19 +23,50 @@ const NoticeListPage = () => {
   }, []);
 
   return (
-    <>
-      <div>
-        {notice.map((value) => (
+    <Layout>
+      <header>
+        <h1>[Notice List]</h1>
+      </header>
+
+      <main>
+        {notice.slice(offset, offset + notices).map((value) => (
           <div key={value.id}>
             <h1>{value.title}</h1>
           </div>
         ))}
-      </div>
-      <div>
-        <button>next Page</button>
-      </div>
-    </>
+      </main>
+
+      <footer>
+        <NoticePagination
+          total={notice.length}
+          notices={notices}
+          page={page}
+          setPage={setPage}
+        />
+      </footer>
+    </Layout>
   );
+  //   <>
+  //     <div>
+  //       {notice.map((value) => (
+  //         <div key={value.id}>
+  //           <h1>{value.title}</h1>
+  //         </div>
+  //       ))}
+  //     </div>
+  //     <div>
+  //       <button>next Page</button>
+  //     </div>
+  //   </>
+  // );
 };
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  max-width: 800px;
+  margin: 0 auto;
+`;
 
 export default NoticeListPage;
---
+++ src/widgets/NoticePagination.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+function Pagination({ total, notices, page, setPage }) {
+  const numPages = Math.ceil(total / notices);
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          &lt;
+        </button>
+        {Array(numPages)
+          .fill()
+          .map((_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => setPage(i + 1)}
+              aria-current={page === i + 1 ? "page" : null}
+            >
+              {i + 1}
+            </button>
+          ))}
+        <button onClick={() => setPage(page + 1)} disabled={page === numPages}>
+          &gt;
+        </button>
+      </nav>
+    </>
+  );
+}
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: left;
+  align-items: left;
+  gap: 4px;
+  margin: 16px;
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 7px;
+  padding: 8px;
+  margin: 0;
+  background: black;
+  color: white;
+  font-size: 1rem;
+
+  &:hover {
+    background: blue;
+    cursor: pointer;
+    transform: translateY(-2px);
+  }
+
+  &[disabled] {
+    background: grey;
+    cursor: revert;
+    transform: revert;
+  }
+
+  &[aria-current] {
+    background: skyblue;
+    font-weight: bold;
+    cursor: revert;
+    transform: revert;
+  }
+`;
+
+export default Pagination;
