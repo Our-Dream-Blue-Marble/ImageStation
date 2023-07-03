f7f8031e1d13e7d0ebbf57e6b0ab23123dd6d6ec
greentea991212 <bewater991212@gmail.com>
2023-07-03 11:34:45 +0900

    feat: 로딩 상태 받기
    로그인 상태에 따라 로딩 상태 변화
    
    (#1)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import { authService } from "fbase";
+import AppFunctions from "functions/AppFunctions";
+import React, { useEffect, useState } from "react";
 import AppRouter from "routes/AppRouter";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObject, setUserObject] = useState(null);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        setUserObject(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setIsLoading(false);
+    });
+  }, []);
   return (
-    <div className="App">
-      <AppRouter isLoggedIn={true} />
-    </div>
+    <>
+      {isLoading ? (
+        "Looading..."
+      ) : (
+        <div className="App">
+          <AppRouter isLoggedIn={isLoggedIn} />
+        </div>
+      )}
+    </>
   );
 }
 
