d6850f22d380678dc258339f606976de4e2d855e
ssuni00 <run9986@naver.com>
2023-07-04 17:10:12 +0900

    feat: Admin계정=true일때 글쓰기 form 생성
    Admin계정일 때 글 작성하는 form 만듦. 하지만 두개의 form을 하나로
    합치는 과정은 아직 구현안됨
    
    (#17)

--- src/pages/AdminNoticeWritePage.js
+++ src/pages/AdminNoticeWritePage.js
@@ -1,5 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { dbService } from "fbase";
+import { async } from "q";
+
 const AdminNoticeWritePage = () => {
-  return <>AdminNoticePage</>;
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+
+  const onSubmit = async (event) => {
+    if (postTitle === "" || postBody === "") {
+      return;
+    }
+    event.preventDefault();
+
+    await dbService.collection("notice").add({
+      title: postTitle,
+      body: postBody,
+      data: Date.now(),
+      writer: "admin",
+    });
+    setPostTitle("");
+    setPostBody("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { title, body },
+    } = event;
+    setPostTitle(title);
+    setPostBody(body);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          title={postTitle}
+          onChange={onChange}
+          type="text"
+          placeholder="제목을 입력하세요"
+          maxLength={120}
+        />
+        <input type="submit" />
+      </form>
+      <br />
+      <form onSubmit={onSubmit}>
+        <input
+          body={postBody}
+          onChange={onChange}
+          type="text"
+          placeholder="게시글을 입력하세요"
+          maxLength={2500}
+        />
+        <input type="submit" body="&rarr;" />
+      </form>
+    </div>
+  );
 };
 
 export default AdminNoticeWritePage;
