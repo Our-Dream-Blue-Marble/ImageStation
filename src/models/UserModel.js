df0fa42b8f56409316602873d322d3f6fcd2bc3e
AnJw-HGU <jingglenonggang@gmail.com>
2023-07-05 11:33:14 +0900

    feat: UserModel 수정 및 Converter 추가
    firestore에서 데이터를 읽고 쓰고 지우고 업데이트를 할 때 converter를 사용하기 위해 형태 변화 및 추가를 함
    
    (#19)

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,13 +1,54 @@
-const UserModel = (data) => {
-  return {
-    email: data.email,
-    name: data.name,
-    phoneNumber: data.phoneNumber,
-    role: data.role,
-    isReceiveMail: data.isReceiveMail,
-    signInDate: data.signInDate,
-    logInDate: data.logInDate,
-  };
+class UserModel {
+  constructor(
+    uid,
+    email,
+    name,
+    phoneNumber,
+    role,
+    isReceiveMail,
+    signInDate,
+    logInDate
+  ) {
+    this.uid = uid ?? this.uid;
+    this.email = email ?? this.email;
+    this.name = name ?? this.name;
+    this.phoneNumber = phoneNumber ?? this.phoneNumber;
+    this.role = role ?? this.role;
+    this.isReceiveMail = isReceiveMail ?? this.isReceiveMail;
+    this.signInDate = signInDate ?? this.signInDate;
+    this.logInDate = logInDate ?? this.logInDate;
+  }
+  toData() {
+    return {
+      uid: this.uid,
+      email: this.email,
+      name: this.name,
+      phoneNumber: this.phoneNumber,
+      role: this.role,
+      isReceiveMail: this.isReceiveMail,
+      signInDate: this.signInDate,
+      logInDate: this.logInDate,
+    };
+  }
+}
+
+export const UserModelConveter = {
+  toFirestore: function (data) {
+    return {
+      uid: data.uid,
+      email: data.email,
+      name: data.name,
+      phoneNumber: data.phoneNumber,
+      role: data.role,
+      isReceiveMail: data.isReceiveMail,
+      signInDate: data.signInDate,
+      logInDate: data.logInDate,
+    };
+  },
+  fromFirestore: function (snapshot, options) {
+    const data = snapshot.data(options);
+    return new UserModel(data);
+  },
 };
 
 export default UserModel;
