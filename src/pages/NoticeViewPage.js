17b807c0f3b80cffcca873e1339e9e7afb389864
eu2goo <eu2goo@gmail.com>
2023-07-10 14:16:13 +0900

    feat: 개별 공지사항 스타일 입히기
    개별 공지사항에 border에 맞춰서 폰트 스타일 입히기
    
    (#69)

--- src/pages/NoticeViewPage.js
+++ src/pages/NoticeViewPage.js
@@ -7,6 +7,7 @@ import {
   onDeleteNoticeClick,
 } from "functions/NoticeFunction";
 import { readNoticeDocument } from "repositories/NoticeRepository";
+import "styles/NoticeViewStyle.scss";
 
 const NoticeViewPage = ({ isAdmin }) => {
   const location = useLocation();
@@ -33,45 +34,50 @@ const NoticeViewPage = ({ isAdmin }) => {
       {noticeViewObj === null ? (
         <>Loading...</>
       ) : (
-        <div>
-          공지번호: {noticeViewObj.id} <br />
-          공지제목: {noticeViewObj.title}
-          <br />
-          <>
-            {noticeViewObj.attachment ? (
-              <a href={noticeViewObj.attachment}> 첨부 파일 </a>
-            ) : (
-              <>첨부파일 없음</>
-            )}
-          </>
-          <br />
-          공지 글: {noticeViewObj.body}
-          <br />
-          작성 날짜: {getNoticeWrittenDate(noticeViewObj)}
-          <br />
-          <button onClick={() => navigate(NoticeListRouteName)}>
-            리스트로 돌아가기
-          </button>
-          {isAdmin ? (
+        <>
+          <div className="noticeViewBoxContainer">
+            <div className="noticeViewTitle">{noticeViewObj.title}</div>
+            <div className="noticeViewRightText">
+              {getNoticeWrittenDate(noticeViewObj)}
+            </div>
+            <div className="noticeViewRightText">no. {noticeViewObj.id}</div>
+            <div className="noticeViewRightText">
+              조회수 {noticeViewObj.view}
+            </div>
             <>
-              <button
-                onClick={() =>
-                  navigate(`${NoticeListRouteName}/update/${id}`, {
-                    state: { data: noticeViewObj },
-                  })
-                }>
-                수정하기
-              </button>
-              <button
-                onClick={() => {
-                  onDeleteNoticeClick(id);
-                  navigate(NoticeListRouteName);
-                }}>
-                삭제하기
-              </button>
+              {noticeViewObj.attachment ? (
+                <div className="noticeViewAttachmentText">
+                  <a href={noticeViewObj.attachment}> 첨부 파일 </a>
+                </div>
+              ) : (
+                <>첨부파일 없음</>
+              )}
             </>
-          ) : null}
-        </div>
+            <div className="noticeViewBodyText">{noticeViewObj.body}</div>
+            <button onClick={() => navigate(NoticeListRouteName)}>
+              리스트로 돌아가기
+            </button>
+            {isAdmin ? (
+              <>
+                <button
+                  onClick={() =>
+                    navigate(`${NoticeListRouteName}/update/${id}`, {
+                      state: { data: noticeViewObj },
+                    })
+                  }>
+                  수정하기
+                </button>
+                <button
+                  onClick={() => {
+                    onDeleteNoticeClick(id);
+                    navigate(NoticeListRouteName);
+                  }}>
+                  삭제하기
+                </button>
+              </>
+            ) : null}
+          </div>
+        </>
       )}
     </>
   );
