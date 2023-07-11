23aff53b40e5951075bea57bf1450df9bc8944fc
greentea991212 <bewater991212@gmail.com>
2023-07-11 17:49:03 +0900

    feat: 로그인 화면 구현 완료
    scss파일 작성 완료 및 아이디/클래스 적용
    
    close #67

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -24,65 +24,67 @@ const LogInPage = () => {
   });
 
   return (
-    <div className="login container">
-      <div className="login container contents">
-        <p>로그인</p>
-        <form
-          onSubmit={async (e) => {
-            await onUserEmailAndPasswordSubmit(
-              e,
-              userEmail,
-              userPassword,
-              isNewUser,
-              setIsNewUser
-            ).then((result) => {
-              if (result) {
-                navigate(HomeRouteName);
-              }
-            });
-          }}
-        >
-          <input
-            className="login emailandpasswordOntextbox"
-            name="userEmail"
-            type="email"
-            placeholder="학교 이메일"
-            required
-            value={userEmail}
-            onChange={(e) => onUserEmailOrPasswordChange(e, setUserEmail)}
-          />
-          <input
-            className="login emailandpasswordOntextbox"
-            name="userPassword"
-            type="password"
-            placeholder="비밀번호"
-            required
-            value={userPassword}
-            onChange={(e) => onUserEmailOrPasswordChange(e, setUserPassword)}
-          />
-          <div className="saveIdContainer">
-            <input className="saveIdCheckBox" type="checkbox" />
-            <label>아이디 저장</label>
+    <div className="LoginBody">
+      <div className="LoginContainer">
+        <div className="Contents">
+          <span>로그인</span>
+          <form
+            onSubmit={async (e) => {
+              await onUserEmailAndPasswordSubmit(
+                e,
+                userEmail,
+                userPassword,
+                isNewUser,
+                setIsNewUser
+              ).then((result) => {
+                if (result) {
+                  navigate(HomeRouteName);
+                }
+              });
+            }}
+          >
+            <input
+              className="InputTextBox"
+              name="userEmail"
+              type="email"
+              placeholder="학교 이메일"
+              required
+              value={userEmail}
+              onChange={(e) => onUserEmailOrPasswordChange(e, setUserEmail)}
+            />
+            <input
+              className="InputTextBox"
+              name="userPassword"
+              type="password"
+              placeholder="비밀번호"
+              required
+              value={userPassword}
+              onChange={(e) => onUserEmailOrPasswordChange(e, setUserPassword)}
+            />
+            <div className="SaveIdContainer">
+              <input type="checkbox" />
+              <label>아이디 저장</label>
+            </div>
+            <input
+              className="SubmitButton"
+              type="submit"
+              value={isNewUser ? "Sign In" : "로그인"}
+            />
+          </form>
+          <div className="UpdateAndSigninButtonsContainer">
+            <button
+              className="UpdateAndSigninButtons"
+              onClick={() => navigate(UpdatePasswordPageRouteName)}
+            >
+              비밀번호 찾기
+            </button>
+            <button
+              className="UpdateAndSigninButtons"
+              onClick={() => navigate(SignInRouteName)}
+            >
+              회원가입
+            </button>
           </div>
-          <input
-            className="login submitButton"
-            type="submit"
-            value={isNewUser ? "Sign In" : "로그인"}
-          />
-        </form>
-        <div className="updateAndSigninButtonsContainer">
-          <button
-            className="updateAndSigninButtons"
-            onClick={() => navigate(UpdatePasswordPageRouteName)}
-          >
-            비밀번호 찾기
-          </button>
-          <button
-            className="updateAndSigninButtons"
-            onClick={() => navigate(SignInRouteName)}
-          >
-            회원가입
-          </button>
         </div>
       </div>
     </div>
