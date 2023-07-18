32187f473a3187142a3c65288e316eea0c962d0f
ssuni00 <run9986@naver.com>
2023-07-18 17:39:49 +0900

    feat: 카드 flip 애니메이션 완성
    카드 hover시에 애니메이션 추가
    
    (#141)

--- src/pages/OrderCategoryPage.js
+++ src/pages/OrderCategoryPage.js
@@ -7,75 +7,77 @@ const OrderCategoryPage = () => {
   return (
     <div className="OrderCategoryPageLayout">
       <div className="cardLayout">
-        <div className="cardContainer">
-          <div className="card front">
-            <div className="contents">일반</div>
-          </div>
-          <div
-            className="card back"
-            onClick={() => navigate(OrderPageRouteName)}
-          >
-            <div className="contents">뒷면</div>
-          </div>
-        </div>
-
-        <div className="cardContainer">
-          <div className="card front">
-            <div className="contents">제본</div>
-          </div>
-          <div
-            className="card back"
-            onClick={() => navigate(OrderPageRouteName)}
-          >
-            <div className="contents">뒷면</div>
+        <div className="flip">
+          <div className="card">
+            <div className="front">
+              <div className="frontCardText">일반</div>
+            </div>
+            <div className="back" onClick={() => navigate(OrderPageRouteName)}>
+              <div className="frontCardText">일반</div>
+              <div className="backCardText">
+                간단한 사무용, 포스터, 카다로그 등 추천합니다!
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="cardContainer">
-          <div className="card front">
-            <div className="contents">라벨지</div>
-          </div>
-          <div
-            className="card back"
-            onClick={() => navigate(OrderPageRouteName)}
-          >
-            <div className="contents">뒷면</div>
-          </div>
-        </div>
-
-        <div className="cardContainer">
-          <div className="card front">
-            <div className="contents">실사대형출력</div>
-          </div>
-          <div
-            className="card back"
-            onClick={() => navigate(OrderPageRouteName)}
-          >
-            <div className="contents">뒷면</div>
+        <div className="flip">
+          <div className="card">
+            <div className="front">
+              <div className="frontCardText">제본</div>
+            </div>
+            <div className="back" onClick={() => navigate(OrderPageRouteName)}>
+              <div className="frontCardText">제본</div>
+              <div className="backCardText">제본 설명 블라블라!</div>
+            </div>
           </div>
         </div>
 
-        <div className="cardContainer">
-          <div className="card front">
-            <div className="contents">사진인화</div>
-          </div>
-          <div
-            className="card back"
-            onClick={() => navigate(OrderPageRouteName)}
-          >
-            <div className="contents">뒷면</div>
+        <div className="flip">
+          <div className="card">
+            <div className="front">
+              <div className="frontCardText">라벨지</div>
+            </div>
+            <div className="back" onClick={() => navigate(OrderPageRouteName)}>
+              <div className="frontCardText">라벨지</div>
+              <div className="backCardText">라벨지 설명 블라블라!</div>
+            </div>
           </div>
         </div>
 
-        <div className="cardContainer">
-          <div className="card front">
-            <div className="contents">기타</div>
+        <div className="flip">
+          <div className="card">
+            <div className="front">
+              <div className="frontCardText">실사대형출력</div>
+            </div>
+            <div className="back" onClick={() => navigate(OrderPageRouteName)}>
+              <div className="frontCardText">실사대형출력</div>
+              <div className="backCardText">실사대형출력 설명 블라블라!</div>
+            </div>
           </div>
-          <div
-            className="card back"
-            onClick={() => navigate(OrderPageRouteName)}
-          >
-            <div className="contents">뒷면</div>
+        </div>
+
+        <div className="flip">
+          <div className="card">
+            <div className="front">
+              <div className="frontCardText">사진인화</div>
+            </div>
+            <div className="back" onClick={() => navigate(OrderPageRouteName)}>
+              <div className="frontCardText">사진인화</div>
+              <div className="backCardText">사진인화 설명 블라블라!</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flip">
+          <div className="card">
+            <div className="front">
+              <div className="frontCardText">기타</div>
+            </div>
+            <div className="back" onClick={() => navigate(OrderPageRouteName)}>
+              <div className="frontCardText">기타</div>
+              <div className="backCardText">기타 설명 블라블라!</div>
+            </div>
           </div>
         </div>
       </div>
