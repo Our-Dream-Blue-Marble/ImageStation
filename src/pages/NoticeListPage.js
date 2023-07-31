729472c91758c8f1d177e095e8f9068cd28a8115
eu2goo <eu2goo@gmail.com>
2023-07-31 16:19:47 +0900

    refactor: 공지사항 검색 시 엔터 입력 없이도 실시간으로 검색 가능하게 수정
     공지사항 검색 할 때 엔터 입력 없이 실시간으로 검색 해서 해당 검색어를 가진 공지사항이 나오게 수정
    
    (#260)

--- src/pages/NoticeListPage.js
+++ src/pages/NoticeListPage.js
@@ -1,15 +1,8 @@
 import React, { useState, useEffect } from "react";
-import NoticePagination from "../widgets/NoticePagination";
 import { useLocation, useNavigate } from "react-router-dom";
-import {
-  NoticeAllRouteName,
-  NoticeListRouteName,
-  NoticeWriteRouteName,
-} from "routes/RouteName";
+import { NoticeAllRouteName, NoticeListRouteName } from "routes/RouteName";
 import { getNoticeList, getNoticeWrittenDate } from "functions/NoticeFunction";
 import "styles/NoticeListStyle.scss";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Pagination } from "swiper";
 
 const NoticeListPage = () => {
   const location = useLocation();
@@ -38,8 +31,7 @@ const NoticeListPage = () => {
         <div className="noticeListHead">이미지스테이션의 소식을 전해드려요</div>
         <div
           className="noticeListAll"
-          onClick={() => navigate(NoticeAllRouteName, { state: false })}
-        >
+          onClick={() => navigate(NoticeAllRouteName, { state: false })}>
           모든 공지
         </div>
       </div>
@@ -52,8 +44,7 @@ const NoticeListPage = () => {
                 navigate(NoticeListRouteName + "/" + value.id, {
                   state: { data: value },
                 })
-              }
-            >
+              }>
               <div className=" noticeListTitle">{value.title}</div>
               <div className=" noticeListDate">
                 {getNoticeWrittenDate(value)}
