62168dc5589b534c483009ce08d50e61b861fef7
ssuni00 <run9986@naver.com>
2023-08-01 14:57:24 +0900

    fix: 공지사항 animation 추가
    스크롤 다운시 공지사항 위에서 아래로
    hover시 공지사항 위로 조금 띄우기
    
    (#266)

--- src/pages/NoticeListPage.js
+++ src/pages/NoticeListPage.js
@@ -29,32 +29,34 @@ const NoticeListPage = () => {
     <div className="noticeListLayout">
       <div className="noticeHead">
         <div className="noticeListHead">공지사항</div>
-        <div className="noticeListExp">이미지스테이션의 소식을 전해드려요</div>
+        <div className="noticeListExp">
+          이미지스테이션의 새로운 소식을 전해드려요
+        </div>
 
-        <div className="AllBtnContainer">
-          <div
-            className="noticeListAll"
-            onClick={() => navigate(NoticeAllRouteName, { state: false })}
-          >
-            더보기
-          </div>
+        <div
+          className="AllBtnContainer"
+          onClick={() => navigate(NoticeAllRouteName, { state: false })}
+        >
+          <div className="noticeListAll">더보기</div>
           <div className="noticeAllArrow"></div>
         </div>
       </div>
       <div className="noticeBoxContainer">
-        {notice.slice(offset, offset + limit).map((value) => (
-          <div className="cardNotice" key={value.id}>
-            <div
-              className="contentCard"
-              onClick={() =>
-                navigate(NoticeListRouteName + "/" + value.id, {
-                  state: { data: value },
-                })
-              }
-            >
-              <div className=" noticeListTitle">{value.title}</div>
-              <div className=" noticeListDate">
-                {getNoticeWrittenDate(value)}
+        {notice.slice(0, 4).map((value) => (
+          <div className="fadeInAnimation">
+            <div className="cardNotice" key={value.id}>
+              <div
+                className="contentCard"
+                onClick={() =>
+                  navigate(NoticeListRouteName + "/" + value.id, {
+                    state: { data: value },
+                  })
+                }
+              >
+                <div className=" noticeListTitle">{value.title}</div>
+                <div className=" noticeListDate">
+                  {getNoticeWrittenDate(value)}
+                </div>
               </div>
             </div>
           </div>
