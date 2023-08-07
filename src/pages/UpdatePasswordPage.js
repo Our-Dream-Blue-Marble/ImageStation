fdb43deafa296caf3661f773e74fb35f94b4739e
AnJw-HGU <jingglenonggang@gmail.com>
2023-08-07 13:44:26 +0900

    feat: 비밀번호 재설정 페이지 디자인 수정
    문구 추가 및 수정, 적용되지 않은 버튼 스타일 구현
    
    (#306)

--- src/pages/UpdatePasswordPage.js
+++ src/pages/UpdatePasswordPage.js
@@ -8,48 +8,48 @@ import { useNavigate } from "react-router-dom";
 import { logInRouteName, SignInRouteName } from "routes/RouteName";
 
 const UpdatePasswordPage = () => {
-  const [email, setEmail] = useState("");
   const navigate = useNavigate();
+  const [userEmail, setUserEmail] = useState("");
 
   return (
-    <div className="UpdatePassword">
-      <div className="UpdatePassWordContainer">
-        <h1>비밀번호 재설정</h1>
-        <p>비밀번호 재설정 링크가 회원님의 메일로 발송됩니다!</p>
-        <form
-          onSubmit={(event) => onUpdatePasswordSubmitWithEmail(event, email)}
-        >
-          <div className="InputDataContainer">
-            <label>학교 이메일</label>
-            <input
-              className="InputTextBox"
-              onChange={(e) => onEmailChange(e, setEmail)}
-              name="email"
-              type="email"
-              placeholder="handong.ac.kr / handong.edu"
-              value={email}
-            />
-            <input type="submit" value="비밀번호 재설정" />
-            <ul>
-              <li>
-                <label>
-                  로그인하고 싶은가요?
-                  <button onClick={() => navigate(logInRouteName)}>
-                    로그인 하기
-                  </button>
-                </label>
-              </li>
-              <li>
-                <label>
-                  회원가입은 하셨나요?
-                  <button onClick={() => navigate(SignInRouteName)}>
-                    회원가입 하기
-                  </button>
-                </label>
-              </li>
-            </ul>
+    <div className="updatePassword-background">
+      <div className="updatePassWord-container">
+        <h1>비밀번호를 잊으셨나요?</h1>
+        <p>
+          비밀번호 재설정 링크가 회원님의 메일로 발송됩니다!
+          <br />
+          스팸메일함도 확인해주세요.
+        </p>
+        <div className="InputDataContainer">
+          <label>학교 이메일</label>
+          <input
+            className="InputTextBox"
+            onChange={(e) => onEmailChange(e, setUserEmail)}
+            name="email"
+            type="email"
+            placeholder="handong.ac.kr / handong.edu"
+            value={userEmail}
+          />
+          <input
+            type="submit"
+            value="비밀번호 재설정"
+            onClick={(e) => onUpdatePasswordSubmitWithEmail(e, userEmail)}
+          />
+          <div className="otherPage-routing-buttons">
+            <div
+              className="otherPage-routing-button"
+              onClick={() => navigate(logInRouteName)}
+            >
+              로그인하고 싶으신가요? <span>로그인</span>
+            </div>
+            <div
+              className="otherPage-routing-button"
+              onClick={() => navigate(SignInRouteName)}
+            >
+              회원가입 하셨나요? <span>회원가입</span>
+            </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
