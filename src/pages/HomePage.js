b39d2ede1f54f292dc28ccc67e261719e842aa36
ssuni00 <run9986@naver.com>
2023-08-07 15:14:57 +0900

    feat: 아래버튼클릭시 공지사항 보이게끔
    버튼에 onclick추가
    
    (#303)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import { scrollToSection } from "functions/AppFunction";
 import { ReactComponent as HomeDownIconAsset } from "assets/icons/HomeDownIconAsset.svg";
-import { ReactComponent as HomePaperAsset } from "assets/HomeDownIconAsset.svg";
+import { ReactComponent as HomePaperAsset } from "assets/HomePaperAsset.svg";
 import "styles/HomeStyle.scss";
 import NoticeListPage from "./NoticeListPage";
 import { useEffect, useState } from "react";
@@ -17,12 +17,21 @@ const HomePage = ({ elementRef }) => {
   return (
     <div className="HomeLayout">
       <div className="HomeStyle">
+        {/* <HomePaperAsset className="paperFalling" /> */}
         <div className="Title">이미지스테이션을 온라인 예약으로</div>
         <div className="SubTitle">
           몇번의 클릭으로 어느 곳에서 쉽고 빠른 프린팅 경험을 제공합니다.
         </div>
         <div className="ButtonText">이미지스테이션의 소식을 듣고 싶나요?</div>
-        <button className="ButtonIcon">
+        <button
+          className="ButtonIcon"
+          onClick={() => {
+            window.scrollTo({
+              top: 550,
+              //behavior: "smooth",
+            });
+          }}
+        >
           <HomeDownIconAsset />
         </button>
       </div>
