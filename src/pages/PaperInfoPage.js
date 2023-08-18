ebcca15860a6d0f4af3308ab8cf5d78af0d64a6b
ssuni00 <run9986@naver.com>
2023-08-18 14:28:50 +0900

    refactor: 종이정보 설명수정
    종이정보 설명수정
    
    (#78)

--- src/pages/PaperInfoPage.js
+++ src/pages/PaperInfoPage.js
@@ -19,7 +19,7 @@ const PaperInfoPage = () => {
             <div className="afterHoverContentBox">
               <div className="paperWeight">220g/m2</div>
               <div className="paperExplain">
-                높은 백색도로 종이가 화사한 특징
+                명함 용도로 자주 쓰이는 종이로 화사함이 느껴지는 특수용지
               </div>
             </div>
           </div>
@@ -28,13 +28,13 @@ const PaperInfoPage = () => {
         <div className="paperCard">
           <div className="contentBox">
             <div className="beforeHoverContentBox">
-              <div className="title">랑데부</div>
+              <div className="title">랑데뷰</div>
               <div className="maxSize">최대크기 A3</div>
             </div>
             <div className="afterHoverContentBox">
               <div className="paperWeight">210g/m2</div>
               <div className="paperExplain">
-                명함용지로 자주 쓰이는 종이로 높은 백색도로 종이가 화사한 특징
+                톡톡한 질감과 터치감이 있는 종이로 고급스러운 용지
               </div>
             </div>
           </div>
@@ -49,7 +49,7 @@ const PaperInfoPage = () => {
             <div className="afterHoverContentBox">
               <div className="paperWeight">200g/m2 120g/m2</div>
               <div className="paperExplain">
-                명함용지로 자주 쓰이는 종이로 높은 백색도로 종이가 화사한 특징
+                무광처리로 은은한 멋이있는 고급 용지
               </div>
             </div>
           </div>
@@ -64,7 +64,7 @@ const PaperInfoPage = () => {
             <div className="afterHoverContentBox">
               <div className="paperWeight">209g/m2</div>
               <div className="paperExplain">
-                명함용지로 자주 쓰이는 종이로 높은 백색도로 종이가 화사한 특징
+                질감이 부드럽고 탄성이 좋아 이미지의 섬세한 표현이 가능한 용지
               </div>
             </div>
           </div>
@@ -79,7 +79,7 @@ const PaperInfoPage = () => {
             <div className="afterHoverContentBox">
               <div className="paperWeight">150g/m2</div>
               <div className="paperExplain">
-                명함용지로 자주 쓰이는 종이로 높은 백색도로 종이가 화사한 특징
+                광택이 나며 고급스러움을 나타내는데에 사용되는 용지
               </div>
             </div>
           </div>
@@ -93,9 +93,7 @@ const PaperInfoPage = () => {
             </div>
             <div className="afterHoverContentBox">
               <div className="paperWeight">150g/m2 120g/m2 100g/m2 80g/m2</div>
-              <div className="paperExplain">
-                명함용지로 자주 쓰이는 종이로 높은 백색도로 종이가 화사한 특징
-              </div>
+              <div className="paperExplain">흔히 사용되는 A4 복사용지</div>
             </div>
           </div>
         </div>
@@ -108,9 +106,7 @@ const PaperInfoPage = () => {
             </div>
             <div className="afterHoverContentBox">
               <div className="paperWeight">220g/m2</div>
-              <div className="paperExplain">
-                명함용지로 자주 쓰이는 종이로 높은 백색도로 종이가 화사한 특징
-              </div>
+              <div className="paperExplain">색이 다양한 용지</div>
             </div>
           </div>
         </div>
