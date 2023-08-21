d434da594adc5644f4e2405e92c8cac2b345b8a3
greentea991212 <bewater991212@gmail.com>
2023-08-21 18:00:15 +0900

    종이에 대한 상세정보 기입
    종이에 대한 상세 정보 기입 및 이에 따른 디자인 변경사항에 대한 반영
    
    (#441)

--- src/widgets/PopUpPaperInfo.js
+++ src/widgets/PopUpPaperInfo.js
@@ -17,8 +17,8 @@ export function PopUpPaperInfo(isPaperInfoPopUp, getPaperInfoPopUp) {
                 </label>
                 <div className="sizeInfo">최대크기A3</div>
                 <div className="detailInfo">
-                  높은 백색도로 화사함을 느낄 수 있으며, 인쇄 선명성이 매우
-                  뛰어나 생생한 비쥬얼 표현에 적합한 용지
+                  특유의 자연스럽고 촉촉한 질감이 특징이며, 잉크 건조성이 매우
+                  뛰어나 생생한 비쥬얼 표현에 적합한 고급 용지.
                 </div>
               </div>
               <div className="EachInfoContainer">
@@ -28,8 +28,8 @@ export function PopUpPaperInfo(isPaperInfoPopUp, getPaperInfoPopUp) {
                 </label>
                 <div className="sizeInfo">최대크기A3</div>
                 <div className="detailInfo">
-                  질감과 터치감있는 표면으로 인쇄물에서 고급스러움이 묻어나는
-                  용지
+                  부드러운 감촉, 두터운 느낌, 그리고 건조성이 뛰어난 고급지로서,
+                  높은 퀄리티를 요구하는 인쇄물에 적합한 용지.
                 </div>
               </div>
               <div className="EachInfoContainer">
@@ -41,7 +41,8 @@ export function PopUpPaperInfo(isPaperInfoPopUp, getPaperInfoPopUp) {
                 </label>
                 <div className="sizeInfo">최대크기A3</div>
                 <div className="detailInfo">
-                  무광처리로 은은한 멋이 있는 고급 용지
+                  광택이 나는 아트지와는 상반되게 차분한 느낌을 내는
+                  무광택지이며, 백감도와 인쇄광택이 탁월함.
                 </div>
               </div>
               <div className="EachInfoContainer">
@@ -51,34 +52,46 @@ export function PopUpPaperInfo(isPaperInfoPopUp, getPaperInfoPopUp) {
                 </label>
                 <div className="sizeInfo">최대크기A3</div>
                 <div className="detailInfo">
-                  실크처럼 매끄럽고 고운 표면으로 잉크 점착성이 우수한 용지
+                  매끈한 표면의 종이질감으로서, 랑데뷰와 비교하여 종이질감을
+                  드러내지 않는 것을 선호하는 경우 사용되는 용지.
                 </div>
               </div>
               <div className="EachInfoContainer">
                 <label className="PaperInfo1">
                   <p>아트지</p>
-                  <p className="Thickness">220g/m{"\xB2"}</p>
+                  <p className="Thickness">150g/m{"\xB2"}</p>
                 </label>
                 <div className="sizeInfo">최대크기A3</div>
                 <div className="detailInfo">
-                  광택이 나며 고급스러움을 나타낼 때 적합한 용지
+                  밝은 순백색의 광택 프리미엄 용지이며,카달로그, 팜플렛, 포스터,
+                  전단지 등의 인쇄물에 주로 사용됨.
                 </div>
               </div>
               <div className="EachInfoContainer">
                 <label className="PaperInfo1">
                   <p>모조지</p>
-                  <p className="Thickness">220g/m{"\xB2"}</p>
+                  <p className="Thickness">
+                    150g/m{"\xB2"} 120g/m{"\xB2"} 100g/m{"\xB2"} 80g/m{"\xB2"}
+                  </p>
                 </label>
                 <div className="sizeInfo">최대크기A3</div>
-                <div className="detailInfo">흔히 사용되는 A4 복사용지</div>
+                <div className="detailInfo">
+                  흔히 사용되는 A4 사이즈의 용지이며, 주로 서적 인쇄의 본문
+                  용지로 사용됨.
+                </div>
               </div>
               <div className="EachInfoContainer">
                 <label className="PaperInfo1">
                   <p>색지</p>
-                  <p className="Thickness">220g/m{"\xB2"}</p>
+                  <p className="Thickness">
+                    150g/m{"\xB2"} 120g/m{"\xB2"} 100g/m{"\xB2"} 80g/m{"\xB2"}
+                  </p>
                 </label>
                 <div className="sizeInfo">최대크기A4</div>
-                <div className="detailInfo">선명한 색을 갖고있는 용지</div>
+                <div className="detailInfo">
+                  모조지에 미색 염로를 첨가하고 무광 처리한 용지로서, 출판물의
+                  본문용지로 사용됨.
+                </div>
               </div>
               <footer>
                 <button className="closedButton" onClick={getPaperInfoPopUp}>
