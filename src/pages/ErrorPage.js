6ed7fe7e0b7cfdeb24cd6719e6f028956fdfab9c
eu2goo <eu2goo@gmail.com>
2023-08-23 19:09:28 +0900

    feat: 에러 404페이지 css 추가, 라투어 수정
    로그인 없이 이용 시 404페이지가 나오게 라우팅
    
    (#449)

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,12 @@
+import "styles/ErrorStyles.scss";
 const ErrorPage = () => {
   return (
-    <>
-      <div>404 not found</div>
-    </>
+    <div className="ErrorPageLayout">
+      <div className="ErrorPageHeaderText">페이지를 찾을 수 없습니다. </div>
+      <div className="ErrorPageBodyText">
+        페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.
+      </div>
+    </div>
   );
 };
 
