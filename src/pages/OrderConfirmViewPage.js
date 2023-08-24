96ac5a64912d7fe088ab062bc7c6af0d2b0bf172
greentea991212 <bewater991212@gmail.com>
2023-08-24 17:42:23 +0900

    feat: 주문내역확인 페이지의 이미지 사이즈 조정
    vh와 vw에 대한 값을 px값으로 조정
    
    (#441)

--- src/pages/OrderConfirmViewPage.js
+++ src/pages/OrderConfirmViewPage.js
@@ -115,7 +115,8 @@ const OrderConfirmViewPage = ({ isAdmin, userObject }) => {
 
                       <div
                         className="categoryAttachment"
-                        onClick={() => onAttachmentDownloadClick(orderData)}>
+                        onClick={() => onAttachmentDownloadClick(orderData)}
+                      >
                         {orderData.attachmentName}
                       </div>
                       {isAdmin ? (
@@ -253,7 +254,8 @@ const OrderConfirmViewPage = ({ isAdmin, userObject }) => {
               <div className="navigateButtonSection">
                 <button
                   className="navigateButton"
-                  onClick={() => navigate(OrderConfirmListRouteName)}>
+                  onClick={() => navigate(OrderConfirmListRouteName)}
+                >
                   돌아가기
                 </button>
               </div>
