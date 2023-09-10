ea55ef031e99506c114b3d83afe675c4409e8ae8
eu2goo <eu2goo@gmail.com>
2023-09-10 21:38:52 +0900

    feat: 공지사항이 없을 때 없다고 띄우기

--- src/pages/NoticeListPage.js
+++ src/pages/NoticeListPage.js
@@ -8,10 +8,12 @@ const NoticeListPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const [notice, setNotice] = useState([]);
+  const [noticeLoaded, setNoticeLoaded] = useState(false);
 
   const [isNoticeDeleted, setIsNoticeDeleted] = useState(location.state);
   useEffect(() => {
     getNoticeList(setNotice);
+    setNoticeLoaded(true);
   }, []);
   useEffect(() => {
     if (isNoticeDeleted) {
@@ -24,44 +26,53 @@ const NoticeListPage = () => {
   }, [isNoticeDeleted]);
 
   return (
-    <div className="noticeListLayout">
-      <div className="noticeHead">
-        <div className="noticeListHead">공지사항</div>
-        <div className="noticeListExp">
-          이미지스테이션의 새로운 소식을 전해드려요.
+    <>
+      {notice.length === 0 ? (
+        <div className="noticeEmptyLayout">
+          <div className="noticeEmptyHeading">
+            죄송합니다! 아직 올라온 공지가 없습니다.
+          </div>
+          <div className="noticeEmptyBody">공지로 곧바로 찾아뵙겠습니다!</div>
         </div>
+      ) : (
+        <div className="noticeListLayout">
+          <div className="noticeHead">
+            <div className="noticeListHead">공지사항</div>
+            <div className="noticeListExp">
+              이미지스테이션의 새로운 소식을 전해드려요.
+            </div>
 
-        <div
-          className="AllBtnContainer"
-          onClick={() => navigate(NoticeAllRouteName, { state: false })}
-        >
-          <div className="noticeListAll">더보기</div>
-          <div className="noticeAllArrow"></div>
-        </div>
-      </div>
-      <div className="noticeBoxContainer">
-        {notice.slice(0, 4).map((value) => (
-          <div className="fadeInAnimation">
             <div
-              className="cardNotice"
-              key={value.id}
-              onClick={() =>
-                navigate(NoticeListRouteName + "/" + value.id, {
-                  state: { data: value },
-                })
-              }
-            >
-              <div className="contentCard">
-                <pre className=" noticeListTitle">{value.title}</pre>
-                <div className=" noticeListDate">
-                  {getNoticeWrittenDate(value)}
-                </div>
-              </div>
+              className="AllBtnContainer"
+              onClick={() => navigate(NoticeAllRouteName, { state: false })}>
+              <div className="noticeListAll">더보기</div>
+              <div className="noticeAllArrow"></div>
             </div>
           </div>
-        ))}
-      </div>
-    </div>
+          <div className="noticeBoxContainer">
+            {notice.slice(0, 4).map((value) => (
+              <div className="fadeInAnimation">
+                <div
+                  className="cardNotice"
+                  key={value.id}
+                  onClick={() =>
+                    navigate(NoticeListRouteName + "/" + value.id, {
+                      state: { data: value },
+                    })
+                  }>
+                  <div className="contentCard">
+                    <pre className=" noticeListTitle">{value.title}</pre>
+                    <div className=" noticeListDate">
+                      {getNoticeWrittenDate(value)}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default NoticeListPage;
